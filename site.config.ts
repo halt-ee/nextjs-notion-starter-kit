50b428ed826afb7e6ffc1a7eadd6a3048b3012f4
halt-ee <halt-nature-frill@duck.com>
2025-02-07 02:38:55 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '28c2f4db032e40d2893f629da9e5ef5f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
